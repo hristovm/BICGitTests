b4ab180fb91561028bb3032bd3663109e0c9aee5
Martin Hristov <m.hristov@seeburger.com>
2013-03-08 11:01:45 +0200

    HelloGitWorld project initial commit

---
+++ HelloGitWorld/src/com/seeburger/bicng/gittests/HelloGitWorld.java
@@ -0,0 +1,14 @@
+package com.seeburger.bicng.gittests;
+
+public class HelloGitWorld
+{
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)
+	{
+		System.out.println("Hello Git world!");
+	}
+
+}
