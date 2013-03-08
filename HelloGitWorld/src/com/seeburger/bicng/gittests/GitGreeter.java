079ceec506a1659c34f771fb54272997aa881087
Martin Hristov <m.hristov@seeburger.com>
2013-03-08 12:25:37 +0200

    Externalized the greeter message as a field

--- HelloGitWorld/src/com/seeburger/bicng/gittests/GitGreeter.java
+++ HelloGitWorld/src/com/seeburger/bicng/gittests/GitGreeter.java
@@ -1,9 +1,14 @@
 package com.seeburger.bicng.gittests;
 
+/**
+ * Git greeter class
+ */
 public class GitGreeter
 {
+	private String helloWorldMsg = "Hello Git World!";
+
 	public void greet()
 	{
-		System.out.println("Hello Git World!");
+		System.out.println(helloWorldMsg);
 	}
 }
