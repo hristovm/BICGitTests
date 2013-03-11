24bb69759314677e9490d2fe2b818ce840a80abc
Dimitar Todorov <d.todorov@seeburger.com>
2013-03-11 16:13:24 +0200

    added javadoc Signed-off-by: Dimitar Todorov <d.todorov@seeburger.com>

--- Sports/src/com/seeburger/bicng/gittests/sports/Sport.java
+++ Sports/src/com/seeburger/bicng/gittests/sports/Sport.java
@@ -4,5 +4,9 @@ public interface Sport
 {
 	void play();
 
+	/**
+	 * this is a javadoc for the dummiest method ever!
+	 * @param somebodysName
+	 */
 	void playWithSomebody(String somebodysName);
 }
